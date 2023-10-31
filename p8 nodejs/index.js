e58be7dd11d3accc091add75fc5b2475a3343fb1
miriamjimenezj <miriam@ASUMiri>
2023-10-31 18:16:07 +0100

    muestra info

--- p8 nodejs/index.js
+++ p8 nodejs/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const os = require('os');
+const osUtils = require('os-utils');
 
 const port = 3000;
 
@@ -9,8 +10,16 @@ function showSystemInfo() {
     console.log(`Versión de Node.js: ${process.version}`);
 }
 
+//Funcion que muestra por consola la información
+function showInfo() {
+    console.log(`Uso de memoria: ${osUtils.freememPercentage().toFixed(2)}% libre`);
+    console.log(`Tiempo activo del sistema: ${os.uptime().toFixed(0)} segundos`);
+    console.log(`Tiempo activo de Node.js: ${process.uptime().toFixed(0)} segundos`);
+    console.log('------------------');
+}
+
 const server = http.createServer((req, res) => {
-    showSystemInfo();
+    showInfo();
     res.statusCode= 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hello, World!</h1>');
@@ -18,4 +27,5 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`);
+    showSystemInfo();
 });
