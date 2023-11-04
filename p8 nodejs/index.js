3aa417cef00e8eed3298865d387e76463c0065a6
miriamjimenezj <miriam.jimenez2277@gmail.com>
2023-11-05 00:27:42 +0100

    Funciona todo

--- p8 nodejs/index.js
+++ p8 nodejs/index.js
@@ -1,8 +1,12 @@
 const http = require('http');
 const os = require('os');
-const osUtils = require('os-utils');
+const fs = require('fs');
 
 const port = 3000;
+
+// Leer la configuración desde un archivo JSON
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+const interval = config.interval;
 
 // Función para mostrar información del sistema y versión de Node.js
 function showSystemInfo() {
@@ -12,20 +16,28 @@ function showSystemInfo() {
 
 //Funcion que muestra por consola la información
 function showInfo() {
-    console.log(`Uso de memoria: ${osUtils.freememPercentage().toFixed(2)}% libre`);
+    console.log(`Uso de CPU: ${os.loadavg()[0].toFixed(2)}%`);
+    console.log(`Uso de memoria: ${((1 - os.freemem() / os.totalmem()) * 100).toFixed(2)}%`);
     console.log(`Tiempo activo del sistema: ${os.uptime().toFixed(0)} segundos`);
     console.log(`Tiempo activo de Node.js: ${process.uptime().toFixed(0)} segundos`);
     console.log('------------------');
 }
 
+//Funcion que muestra la info de forma periódica
+function periodic(){
+    setInterval(showInfo, interval);
+}
+
+//Creamos servidor httpd
 const server = http.createServer((req, res) => {
-    showInfo();
     res.statusCode= 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<h1>Hello, World!</h1>');
 });
 
+// Iniciar el servidor en el puerto 3000
 server.listen(port, () => {
     console.log(`Server running at port ${port}`);
     showSystemInfo();
+    periodic();
 });
